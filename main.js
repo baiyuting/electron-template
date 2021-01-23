1bb5325371a954341b6cbee2f4565ef1f95a15fa
baiyuting <baiyuting_hi@163.com>
2021-01-23 19:38:01 +0800

    自动更新实现 electron-is-dev判断是否是开发环境

--- main.js
+++ main.js
@@ -2,6 +2,35 @@ const {app, BrowserWindow} = require('electron')
 
 if (require('electron-squirrel-startup')) return;
 
+function update_electron() {
+    const isDev = require('electron-is-dev')
+    if (!isDev) {
+        // 不是在开发环境中
+        const {app, autoUpdater, dialog} = require('electron')
+        const server = 'http://localhost:8080'
+        const url = `${server}/message_pushing`
+
+        autoUpdater.setFeedURL({url})
+        // 每次启动时检查一次
+        autoUpdater.checkForUpdates()
+
+        autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+            autoUpdater.quitAndInstall()
+        })
+
+        autoUpdater.on('error', message => {
+            dialog.showMessageBox({
+                type: 'info',
+                message: "应用更新出错",
+                detail: message
+            })
+            const fs = require("fs")
+            fs.writeFileSync("update_error.log", message, "utf-8") // 写入错误日志
+        })
+
+    }
+
+}
 
 function createWindow() {
     const win = new BrowserWindow({
@@ -15,7 +44,7 @@ function createWindow() {
     win.loadFile('index.html')
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow).then(update_electron)// 创建好窗口之后执行更新程序
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
